032da8eab27ff78ac8c0136dd388d8af9218bb28
pakjkwan <pakjkwan@gmail.com>
2020-10-06 15:34:54 +0900

    1

--- model/crime.py
+++ model/crime.py
@@ -9,6 +9,12 @@ class CrimeModel:
         print(f'baseurl #### {baseurl}')
         self.reader = FileReader()
 
+    def hook_process(self):
+        print('----------- CRIME & POLICE ----------')
+        crime = self.get_crime()
+        print(f'{crime.head()}')
+        self.get_station(crime)
+
 
     def get_crime(self):
         reader = self.reader
@@ -16,10 +22,31 @@ class CrimeModel:
         reader.fname = 'crime_in_seoul.csv'
         reader.new_file()
         crime = reader.csv_to_dframe()
-        print(f'{crime.head()}')
+        return crime
+
+    def get_station(self, crime):
+        reader = self.reader
+        station_names = []
+        for name in crime['관서명']:
+            station_names.append('서울'+str(name[:-1]+'경찰서'))
+        station_addrs = []
+        station_lats = [] # 위도
+        station_lngs = [] # 경도
+        gmaps = reader.create_gmaps()
+        for name in station_names:
+            t = gmaps.geocode(name, language='ko')
+            station_addrs.append(t[0].get('formatted_address'))
+            t_loc = t[0].get('geometry')
+            station_lats.append(t_loc['location']['lat'])
+            station_lngs.append(t_loc['location']['lng'])
+            print(name+'----->' + t[0].get('formatted_address'))
+
+
+
+    
 
 
 if __name__ == '__main__':
     crime = CrimeModel()
-    crime.get_crime()
+    crime.hook_process()
     
