ae9a6d559c577a116151b3f631fd304e1aacf510
pakjkwan <pakjkwan@gmail.com>
2020-10-07 17:16:32 +0900

    17

--- model/fashion_mnist.py
+++ model/fashion_mnist.py
@@ -34,9 +34,6 @@ class FashionMnist:
 
         return [train_images, train_labels, test_images, test_labels]
 
-
-    
-
     def preprocess(self, train_images, train_labels):
         plt.figure(figsize=(10,10))
         for i in range(25):
@@ -77,6 +74,36 @@ class FashionMnist:
         answer = test_labels[index]
         print(f'모델이 예측한 값: {np.argmax(pred)}')
         print(f'정답 : {answer}')
+        return [predictions, test_images, test_labels]
+
+    def plot_image(self, i, predictions_array, true_label, img) ->[]:
+        predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+        plt.grid(False)
+        plt.xticks([])
+        plt.yticks([])
+        plt.imshow(img, cmap=plt.cm.binary)
+
+        prediction_label = np.argmax(predictions_array)
+        if prediction_label == true_label:
+            color = 'blue'
+        else:
+            color = 'red'
+        plt.xlabel('{} : {} %'.format(self.class_names[prediction_label],
+                                        100 * np.max(predictions_array),
+                                        self.class_names[true_label], color
+        ))
+
+    @staticmethod
+    def plot_value_array(i, predictions_array, true_label):
+        predictions_array, true_label = predictions_array[i], true_label[i]
+        plt.grid(False)
+        plt.xticks([])
+        plt.yticks([])
+        thisplot = plt.bar(range(10), predictions_array, color='#777777')
+        plt.ylim([0, 1])
+        prediction_label = np.argmax(predictions_array)
+        thisplot[prediction_label].set_color('red')
+        thisplot[true_label].set_color('blue')
 
 
 
